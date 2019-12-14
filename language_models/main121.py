9abeb6f4e22447284c35d1fcd9a564685d2827bd
AdiKrasin <adikrasin@gmail.com>
2019-12-14 20:39:46 +0200

    created the n gram model

--- language_models/main121.py
+++ language_models/main121.py
@@ -2,7 +2,9 @@ from language_models.models.ngram_model import NgramModel
 
 
 def train_word_lm(data_set, n=2):
-    # TODO NEED TO MAKE SURE THE DATA SET IS BEING SENT AS STRING
+    # TODO WRITE THIS IN THE NOTEBOOK - also in the notebook need to answer the question under 1.2.2
+    # TODO NEED TO MAKE SURE THE DATA SET IS BEING SENT AS STRING - NEED TO UNDERSTAND WHAT IS THE data_set (PATH?
+    #  STRING?)
     my_model = NgramModel(data_set, n)
     my_model.run()
     # TODO DELETE THIS WHEN FINISH TESTING
