13a012d766bf5f8336aa6e21a8e5ed8763cabd15
AdiKrasin <adikrasin@gmail.com>
2019-12-22 09:44:37 +0200

    added curve fitting

--- polynomial_curve_fitting/main22.py
+++ polynomial_curve_fitting/main22.py
@@ -1,11 +1,12 @@
 import numpy as np
 from polynomial_curve_fitting.main21 import generateDataset, f
+import matplotlib.pyplot as plt
 
 
 def OptimizeLS(x, t, M):
-    phi = np.zeros((len(x), M))
+    phi = np.zeros((len(x), M+1))
     for n in range(len(x)):
-        for m in range(M):
+        for m in range(M+1):
             # TODO I AM NOT SURE THAT THIS IS THE MEANING HERE - NEED TO CHECK WITH ELHADAD
             phi[n][m] = pow(x[n], m)
     wls = np.dot(np.dot(np.linalg.inv(np.dot(phi.T, phi)), phi.T), t)
@@ -20,9 +21,16 @@ def compute(cs, x):
 
 
 if __name__ == "__main__":
-    (vector1, vector2) = generateDataset(10, f, 0.03)
-    coefficients = OptimizeLS(vector1, vector2, 10)
-    approx_values = [compute(coefficients, x) for x in vector1]
-    values = [f(x) for x in vector1]
-    print(approx_values)
-    print(values)
+    ns = [1, 3, 5, 10]
+    for n in ns:
+        (vector1, vector2) = generateDataset(10, f, 0.03)
+        coefficients = OptimizeLS(vector1, vector2, n)
+        approx_values = [compute(coefficients, x) for x in vector1]
+        values = [f(x) for x in vector1]
+        area = np.pi * 3
+        plt.scatter(vector1, values, s=area, c='red', alpha=0.5)
+        plt.scatter(vector1, approx_values, s=area, c='blue', alpha=0.5)
+        plt.title('Scatter plot')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.show()
